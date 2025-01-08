8a526586b24ca20b70828c3a313f2bbee085391a
ryantangmj <ryantangmj@gmail.com>
2025-01-08 22:05:37 +0800

    Added smart contract bare bones

---
+++ fintech-hackathon/src/SmartContract.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+
+function SmartContract() {
+    const [inputValue, setInputValue] = useState("");
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+        e.target.style.height = "auto"; // Reset the height to auto to calculate the new height
+        e.target.style.height = e.target.scrollHeight + "px"; // Set the height based on the scroll height
+      };
+  
+    const handleButtonClick = () => {
+      alert("Will add functionality later" ); 
+    };
+  
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>Smart Contract Page</h1>
+        <textarea
+        placeholder="Enter text here"
+        value={inputValue}
+        onChange={handleInputChange}
+        style={{
+          marginBottom: "20px",
+          width: "500px",
+          fontSize: "16px",
+          border: "1px solid #ccc",
+          borderRadius: "5px",
+          resize: "none", 
+          overflow: "hidden",
+        }}
+      />
+        <br />
+        <button
+          onClick={handleButtonClick}
+          style={{
+            padding: "10px 20px",
+            fontSize: "16px",
+            backgroundColor: "#4CAF50",
+            color: "white",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+        >
+          Verify
+        </button>
+      </div>
+    );
+}
+
+export default SmartContract;
--- fintech-hackathon/src/index.js
+++ fintech-hackathon/src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Dashboard from "./Dashboard";
+import SmartContract from "./SmartContract";
+
+const router = createBrowserRouter([
+  {
+      path: "/",
+      element: <Dashboard />
+  },
+  {
+      path: "/smartcontract",
+      element: <SmartContract />
+  }
+]); 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
