617691075d5ae4a860ae77848a0bc9177bacba9f
ryantangmj <ryantangmj@gmail.com>
2025-01-08 22:08:41 +0800

    Updated indentation

--- fintech-hackathon/src/SmartContract.js
+++ fintech-hackathon/src/SmartContract.js
@@ -1,23 +1,22 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
 
 function SmartContract() {
-    const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
 
-    const handleInputChange = (e) => {
-        setInputValue(e.target.value);
-        e.target.style.height = "auto"; // Reset the height to auto to calculate the new height
-        e.target.style.height = e.target.scrollHeight + "px"; // Set the height based on the scroll height
-      };
-  
-    const handleButtonClick = () => {
-      alert("Will add functionality later" ); 
-    };
-  
-    return (
-      <div style={{ textAlign: "center", marginTop: "50px" }}>
-        <h1>Smart Contract Page</h1>
-        <textarea
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+    e.target.style.height = "auto";
+    e.target.style.height = e.target.scrollHeight + "px";
+  };
+
+  const handleButtonClick = () => {
+    alert("Will add functionality later");
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Smart Contract Page</h1>
+      <textarea
         placeholder="Enter text here"
         value={inputValue}
         onChange={handleInputChange}
@@ -27,27 +26,27 @@ function SmartContract() {
           fontSize: "16px",
           border: "1px solid #ccc",
           borderRadius: "5px",
-          resize: "none", 
+          resize: "none",
           overflow: "hidden",
         }}
       />
-        <br />
-        <button
-          onClick={handleButtonClick}
-          style={{
-            padding: "10px 20px",
-            fontSize: "16px",
-            backgroundColor: "#4CAF50",
-            color: "white",
-            border: "none",
-            borderRadius: "5px",
-            cursor: "pointer",
-          }}
-        >
-          Verify
-        </button>
-      </div>
-    );
+      <br />
+      <button
+        onClick={handleButtonClick}
+        style={{
+          padding: "10px 20px",
+          fontSize: "16px",
+          backgroundColor: "#4CAF50",
+          color: "white",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer",
+        }}
+      >
+        Verify
+      </button>
+    </div>
+  );
 }
 
 export default SmartContract;
